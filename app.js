6ce53035d5596c99db8e56d3f268b8a1e49d130a
melocotoneroh <aamarber@gmail.com>
2022-12-20 09:52:42 +0100

    Added produxt detail to the API

--- app.js
+++ app.js
@@ -70,3 +70,17 @@ const productsDB = require("./data/products/productsdb");
 app.post('/products', auth, (request, result) => {
     result.status(200).json(productsDB.products);
 });
+
+app.post('/products/:id?', auth, (request, result) => {
+    console.log('HOLA');
+
+    const productId = request.params.id;
+
+    if(!productId){
+        result.status(404).send('Product not found');
+    }
+
+    const product = productsDB.find(productId);
+
+    result.status(200).json(product);
+});
